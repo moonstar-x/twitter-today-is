c1dcb4b14176c118794d12a7c118e562ba9cfcd5
moonstar-x <christian.lopez99@outlook.com>
2020-08-06 11:17:21 -0500

    Added support for png images.

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const createImagesObject = () => {
-  const images = fs.readdirSync(path.join(__dirname, '../../assets')).filter((file) => file.endsWith('.jpg'));
+  const images = fs.readdirSync(path.join(__dirname, '../../assets')).filter((file) => file.endsWith('.jpg') || file.endsWith('.png'));
   const initial = {};
 
   for (let i = 0; i < 7; i++) {
