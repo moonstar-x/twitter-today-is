88e657107dd34e9b844b9850419aff42f5a19ee2
moonstar-x <christian.lopez99@outlook.com>
2020-08-08 20:34:35 -0500

    Corrected script and workflow file.

--- src/app.js
+++ src/app.js
@@ -1,43 +1,39 @@
 const Twitter = require('twitter');
-const Toolkit = require('actions-toolkit');
+const logger = require('@greencoast/logger');
 const fs = require('fs');
 const path = require('path');
 const { createImagesObject, randomItemFromArray } = require('./utils');
 
 const imagesByDay = createImagesObject();
 
-Toolkit.run((tools) => {
-  tools.log.info('Executing Action...');
-
-  const client = new Twitter({
-    consumer_key: tools.inputs.TWITTER_CONSUMER_KEY,
-    consumer_secret: tools.inputs.TWITTER_CONSUMER_SECRET,
-    access_token_key: tools.inputs.TWITTER_ACCESS_TOKEN,
-    access_token_secret: tools.inputs.TWITTER_ACCESS_SECRET
-  });
-
-  const file = randomItemFromArray(imagesByDay[new Date().getDay()]);
-  const image = fs.readFileSync(path.join(__dirname, '../assets', file));
-
-  client.post('media/upload', { media: image })
-    .then((media) => {
-      tools.log.info(`Uploaded image ${file}`);
-
-      client.post('statuses/update', { status: '', media_ids: media.media_id_string, lat: tools.inputs.COORD_LAT, long: tools.inputs.COORD_LONG })
-        .then((tweet) => {
-          tools.log.info(`Posted tweet with ID: ${tweet.id_str}. Current number of tweets: ${tweet.user.statuses_count + 1}`);
-          tools.exit.success('Finished job.');
-        })
-        .catch((error) => {
-          tools.log.fatal('There was an error on tweet!');
-          tools.exit.failure(error);
-        });
-    })
-    .catch((error) => {
-      tools.log.fatal('There was an error on image upload!');
-      tools.exit.failure(error);
-    });
-}, {
-  event: 'schedule',
-  secrets: ['TWITTER_CONSUMER_KEY', 'TWITTER_CONSUMER_SECRET', 'TWITTER_ACCESS_TOKEN', 'TWITTER_ACCESS_SECRET']
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET
 });
+
+logger.info('Executing Action...');
+
+const file = randomItemFromArray(imagesByDay[new Date().getDay()]);
+const image = fs.readFileSync(path.join(__dirname, '../assets', file));
+
+client.post('media/upload', { media: image })
+  .then((media) => {
+    logger.info(`Uploaded image ${file}`);
+
+    client.post('statuses/update', { status: '', media_ids: media.media_id_string, lat: process.env.COORD_LAT, long: process.env.COORD_LONG })
+      .then((tweet) => {
+        logger.info(`Posted tweet with ID: ${tweet.id_str}. Current number of tweets: ${tweet.user.statuses_count + 1}`);
+      })
+      .catch((error) => {
+        logger.fatal('There was an error on tweet!');
+        logger.fatal(error);
+        process.exit(1);
+      });
+  })
+  .catch((error) => {
+    logger.fatal('There was an error on image upload!');
+    logger.fatal(error);
+    process.exit(1);
+  });
